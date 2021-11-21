e221d6b8f510a9bcbafe7359d4de9d7f82d39659
maxim <blvckonej@gmail.com>
2021-11-21 17:24:39 +0300

    lesson petrichenko practick

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
 "use strict";
 
-let num = 50;
+const nubmer0fFilms = +prompt("Skolko filmov vi uje glyanuli?");
 
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
+console.log(nubmer0fFilms);
 
-// do {
-//   console.log(num);
-//   num++;
-// } while (num < 55);
+const personalMovieDB = {
+  count: nubmer0fFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
 
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        break;
-    }
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Odin iz poslednih filmov?", ""),
+    b = prompt("Na skolko ocenite ego?", "");
 
-    console.log(i);    
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("Done!");
+  } else {
+    console.log("error");
+    i--;
+  }
 }
+
+if (personalMovieDB.count < 10) {
+  console.log("Malo filmov");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Classical zritel");
+} else if (personalMovieDB.count >= 30) {
+  console.log("vi kinoman");
+} else {
+  console.log("Error!");
+}
+
+console.log(personalMovieDB);
